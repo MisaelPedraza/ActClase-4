0a2553ff6b45e7a5187902dd1227ab4b2ef3eb5e
MisaelPedraza <88685012+MisaelPedraza@users.noreply.github.com>
2021-08-23 16:15:16 -0500

    Update exercise.py

--- assignments/14DistanciaDosPuntos/src/exercise.py
+++ assignments/14DistanciaDosPuntos/src/exercise.py
@@ -1,7 +1,13 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    from math import sqrt
+    x1 = int(input('x1= '))
+    y1 = int(input('y1= '))
+    x2 = int(input('x2= '))
+    y2 = int(input('y2= '))
+    d = sqrt(((x2-x1)**2) + ((y2-y1)**2))
+    print('distancia=', d)
 
 if __name__ == '__main__':
     main()
