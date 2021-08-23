b2d1489239dfa19e086cdd05af09fbe023b8523a
MisaelPedraza <88685012+MisaelPedraza@users.noreply.github.com>
2021-08-23 14:45:46 -0500

    Update exercise.py

--- assignments/11PEMDAS/src/exercise.py
+++ assignments/11PEMDAS/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    from math import sqrt
+    a=4
+    b=5
+    oper1 = float(2*(3/4) + 4*(2/3) - 3*(1/5) + 5*(1/2))
+    oper2 = float((2*sqrt(35**2)) + (4*sqrt(36**3)) - (6*sqrt(49**2)))
+    oper3 = float(((a**3) + 2*(b**2)) / (4*a))
+    oper4 = float(((2*(a+b)**2)+4*(a-b)**2)/ (a*b**2))
+    oper5 = float(sqrt((a+b)**2 + 2**(a+b)) / (2*a + 2*b)**2)
+    print(oper1)    
+    print(oper2)
+    print(oper3)
+    print(oper4)
+    print(oper5)
 
 
 
