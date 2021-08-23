112b25535334303ec6775919d0789788e8a1c008
MisaelPedraza <88685012+MisaelPedraza@users.noreply.github.com>
2021-08-23 16:31:43 -0500

    Update exercise.py

--- assignments/17RazonAurea/src/exercise.py
+++ assignments/17RazonAurea/src/exercise.py
@@ -1,7 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    varphi = (1+sqrt(5))/2
+    #print(varphi)
+    n = float(input('Número: '))
+    d = int(input('Decimales a mostrar: '))
+    r = round(n * varphi, d)
+    print('Razón áurea:', r)
 
 if __name__ == '__main__':
     main()
